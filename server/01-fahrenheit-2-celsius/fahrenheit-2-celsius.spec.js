fbb5c0e9774803aa2ab1c8168794263731186101
JOEL KAPLAN <joelkaplan@JOELs-MacBook-Pro.local>
2020-01-27 13:33:10 -0800

    Fix first test

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -7,10 +7,13 @@ describe('the fahrenheit-2-celcius canary spec', () => {
   });
 });
 
-describe('a fahrenheitToCelcius function should', () => {
-  it.todo('return 0 for 32');
-  it.todo('return 10 for 50');
-  it.todo('return 100 for 212');
-  it.todo('return -40 for -40');
-  it.todo('return -273.15 for -459.67');
+describe.only('a fahrenheitToCelcius function should', () => {
+  it('return 0 for 32', () => {
+    console.log(fahrenheitToCelcius(32).should);
+    expect(fahrenheitToCelcius(32)).toBe(0);
+  });
+  it('return 10 for 50');
+  it('return 100 for 212');
+  it('return -40 for -40');
+  it('return -273.15 for -459.67');
 });
